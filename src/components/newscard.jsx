398a1292000b4e16058a775a3f406682d30aa0b9
OhVickie <moi.vhiktoire@gmail.com>
2020-04-28 01:00:45 -0400

    .

--- src/components/newscard.jsx
+++ src/components/newscard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Newscard() {
   return (
     <div className="newscard">
-       
+       <img></img>
       <a href="#" className="btn btn--white btn--animated">Hey</a>
     </div>
   );
