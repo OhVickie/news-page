a550a2abe3bd946d04723e411f5cb51b14444d40
OhVickie <moi.vhiktoire@gmail.com>
2020-05-05 23:57:50 -0400

    ..

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,21 +5,22 @@ import banana from '../img/banana-4415864_1920.jpg';
 import video from '../img/video.mp4';
 import img1 from '../img/bee-2984342_1920.jpg';
 import img2 from '../img/dark-fern-fern-leaves-1216345.jpg';
-import img3 from '../img/sliced-lime.jpg';
+import img3 from '../img/green-leafy-plant-1092197.jpg';
 import img4 from '../img/lighted-flower-bud.jpg';
 import img5 from '../img/green-peas.jpg';
-import img6 from '../img/green-leafy-plant-1092197.jpg';
+import img6 from '../img/plant-with-water.jpg';
+import img7 from '../img/sliced-lime.jpg';
 
 function Home() {
   return (
     <div className="home">
-        <h4 className="webpage_title">bonkers.</h4>
-        <div className="home-content">
-            <h1 className="title-part">
-                <span className="title-part--1">Hello!</span>
-                <span className="title-part--1">My name is Victory Esim.</span>
+        <h4 className="webpage__title">bonkers.</h4>
+        <div className="home__content">
+            <h1 className="title__part">
+                <span className="title__part--1">Hello!</span>
+                <span className="title__part--1">My name is Victory Esim.</span>
             </h1>
-            <p className="title-message">
+            <p className="title__message">
                 I am one of a kind. 
                 I am brilliant, creative and passionate about making stunning and intuitive user interfaces.
             </p>
@@ -27,12 +28,12 @@ function Home() {
                 <a href="https://github.com/OhVickie?tab=repositories" className="more-link">Learn More!</a>
             </p>
         </div>
-        <div className="yellow-div">
-            <div className="learn-more-div"></div>
+        <div className="yellow__div">
+            <div className="learn-more__div"></div>
             <h4 className="aesthetic">Aes.<span>the.</span>tic</h4>
             <p>concerned with beauty or the appreciation of beauty.</p>
         </div>
-        <div className="banana-div">    
+        <div className="banana__div">    
         </div>
         
         {/*This part will be a grid containing newscards*/}
@@ -41,8 +42,8 @@ function Home() {
             <div className="picture__grid">
                 <div className="item item--1"><img src={img5} className="grid-img"></img></div>
                 <div className="item item--2"><img src={img2} className="grid-img"></img></div>
-                <div className="item item--3"><img src={img3} className="grid-img"></img></div>
-                <div className="item item--4"><img src={img6} className="grid-img"></img></div>
+                <div className="item item--3"><img src={img6} className="grid-img"></img></div>
+                <div className="item item--4"><img src={img3} className="grid-img"></img></div>
                 
             </div>
             
@@ -50,8 +51,8 @@ function Home() {
                 <source src={video} type="video/mp4"></source>
             </video>*/}
         </div>
-        <div className="newscard-div">
-            <h4 className="newscard-div-title">View Testimonials</h4>
+        <div className="newscard__div">
+            <h4 className="newscard__div--title">View Testimonials</h4>
             <div className="container-fluid d-flex justify-content-center">
                 
                 <div className="row">
